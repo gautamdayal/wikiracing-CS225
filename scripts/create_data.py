5d3308e311c79d37bd4d8ae88bae9f23d1a779b4
Gautam Dayal <gautam.dayal@yahoo.com>
2022-11-18 20:56:10 -0600

    Added data generation logic and some sample data

--- scripts/create_data.py
+++ scripts/create_data.py
@@ -1,7 +1,15 @@
 from classes import *
 
-node_0 = WikiPage('/wiki/Tea', "Tea")
-print(node_0.make_csv())
+def createSubgraphData(source, depth, breadth):
+    q = []
+    q.append(source)
+    for i in range(depth * breadth):
+        page = q.pop()
+        to_add = page.make_csv()[page.link][:breadth]
+        print(to_add)
+        for item in to_add:
+            q.append(WikiPage(item))
 
-def createSubgraphData(root, breadth, depth): #  function to make subgraph dict
-    pass
+node_0 = WikiPage('/wiki/Gaiwan', "Gaiwan")
+# print(node_0.make_csv())
+createSubgraphData(node_0, 2, 5)
