2b32626bf7570aae3dfdef39d4d6203ba0950268
Gautam Dayal <gautam.dayal@yahoo.com>
2022-11-30 15:13:00 -0600

    Corrected configuration file

---
+++ docs/search/functions_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['parsefromfile_6',['ParseFromFile',['../class_wiki_graph.html#a6d9b6ffc4ff5b32a1ce18c6f623d16ec',1,'WikiGraph']]],
+  ['parsefromfiles_7',['ParseFromFiles',['../class_wiki_graph.html#afa6dfa0c78bb6d05b0179f1b162533aa',1,'WikiGraph']]],
+  ['printedgelist_8',['PrintEdgeList',['../class_wiki_graph.html#a95f621d12c42ee2a08f25b0f1887b667',1,'WikiGraph']]]
+];
