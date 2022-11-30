cb9c6c10dffe4a26efb9a6648f67c43a969fc868
Aryaman Dwivedi <aryaman.dwivedi@hotmail.com>
2022-11-30 21:05:23 +0000

    New test cases just dropped

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -14,3 +14,16 @@ TEST_CASE("ParseFromFileTest1") {
     std::string output = test.PrintEdgeList();
     REQUIRE(output == actual);
 }
+
+TEST_CASE("Shortest Path") {
+    WikiGraph test;
+    test.ParseFromFile("../tests/data/demofile.csv");
+    std::string actual = "a>b\n"
+                        "a>c\n"
+                        "b>c\n"
+                        "b>e\n"
+                        "c>d\n"
+                        "d>e\n";
+    std::vector<std::string> a_to_e = {"a","b","e"};
+    REQUIRE(test.ShortestPath("a", "e") == a_to_e);
+}
