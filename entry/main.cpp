d0d08c9c883753b11899c491f1ad2b6eee857687
Aryaman Dwivedi <aryaman.dwivedi@hotmail.com>
2022-11-30 16:21:09 +0000

    Test Files

--- entry/main.cpp
+++ entry/main.cpp
@@ -11,8 +11,8 @@ int main() {
     //                                     "../data/Tea.csv"};
     std::vector<std::string> file_names = {"../data/Hydrocera.csv",
                                         "../data/Gaiwan.csv",};
-    graph.parse_from_files(file_names);
-    std::string edge_list = graph.print_edge_list();
+    graph.ParseFromFiles(file_names);
+    std::string edge_list = graph.PrintEdgeList();
     std::cout << edge_list << std::endl;
     std::cout << "****** https://graphonline.ru/en/create_graph_by_edge_list ******" << std::endl;
     std::cout << "Use this website to plot the graph" << std::endl;
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,7 +1,16 @@
 #include <catch2/catch_test_macros.hpp>
 #include "wikigraph.h"
 
-TEST_CASE("DEMO") {
+TEST_CASE("ParseFromFileTest1") {
     WikiGraph test;
-    REQUIRE(true);
+    test.ParseFromFile("../tests/data/demofile.csv");
+    std::string actual = "a>b\n"
+                        "a>c\n"
+                        "b>c\n"
+                        "b>e\n"
+                        "c>d\n"
+                        "d>e\n";
+                      
+    std::string output = test.PrintEdgeList();
+    REQUIRE(output == actual);
 }
